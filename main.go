7ba1aa140062c303da56d846ce4df3a7ba04739d
tmsc4zht <79966690+tmsc4zht@users.noreply.github.com>
2021-03-19 01:18:54 +0900

    add goreleaser

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	Version  = "unset"
+	Revision = "unset"
+)
 
 func main() {
-	fmt.Println("hello world")
+	var usageFlag bool
+	var versionFlag bool
+
+	flag.BoolVar(&usageFlag, "usage", false, "print usage")
+	flag.BoolVar(&versionFlag, "version", false, "print version")
+
+	flag.Parse()
+
+	if usageFlag {
+		fmt.Println("open todo")
+		return
+	}
+
+	if versionFlag {
+		fmt.Printf("%s(%s)", Version, Revision)
+		return
+	}
+
 }
