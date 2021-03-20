8fcbcaea49771e8b276910d8d0e737a7e2775468
tmsc4zht <79966690+tmsc4zht@users.noreply.github.com>
2021-03-20 20:57:23 +0900

    add make script

---
+++ make.cmd
@@ -0,0 +1,13 @@
+@rem this code based on https://github.com/zetamatta/nyagos/blob/master/make.cmd
+@powershell "iex((@('')*3+(cat '%~f0'|select -skip 3))-join[char]10)"
+@exit /b %ERRORLEVEL%
+
+$VerbosePreference = "Continue"
+
+$version = (git describe --tags --abbrev=0)
+$revision = (git rev-parse --short HEAD)
+
+Write-Verbose "Build as version='$version' revision='$revision'"
+
+Write-Verbose "$ go build"
+go build -trimpath -ldflags "-s -w -X main.Version=$version -X main.Revision=$revision"
